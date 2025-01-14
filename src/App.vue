efea2814550d5d74bf3b4141541baaf1dddc1849
Maureen-gdd <maureen.grandidier@gmail.com>
2025-01-13 21:35:36 -0500

    🐛[Tailwind] Fix TailwindCSS
    - Added `postcss.config.js` for PostCSS configuration

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import SwimmerAnimation from './components/animations/SwimmerAnimation.vue'
 </script>
 
 <template>
-  <header>
+  <!--<header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header>-->
 
   <main>
-    <TheWelcome />
+    <!--<TheWelcome />-->
+    <SwimmerAnimation />
   </main>
 </template>
 
---
+++ src/components/animations/SwimmerAnimation.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-container bg-blue-500">
+    
+    </div>
+</template>
+
+<style scoped>
+.bg-container {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
